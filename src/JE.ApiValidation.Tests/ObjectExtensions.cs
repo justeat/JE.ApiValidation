99edae03a7f562a7592d05489602d2b3550e6005
Peter Mounce <peter.mounce@just-eat.com>
2015-06-05 17:35:24 +0100

    misc warnings

--- src/JE.ApiValidation.Tests/ObjectExtensions.cs
+++ src/JE.ApiValidation.Tests/ObjectExtensions.cs
@@ -5,17 +5,6 @@ namespace JE.ApiValidation.Tests
 {
     public static class ObjectExtensions
     {
-        public static void SetPrivatePropertyValue<T>(this object obj, string propName, T val)
-        {
-            var t = obj.GetType();
-            if (t.GetProperty(propName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance) == null)
-                throw new ArgumentOutOfRangeException("propName",
-                    string.Format("Property {0} was not found in Type {1}", propName, obj.GetType().FullName));
-            t.InvokeMember(propName,
-                BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.SetProperty | BindingFlags.Instance, null,
-                obj, new object[] { val });
-        }
-
         public static void SetPrivateFieldValue<T>(this object obj, string propName, T val)
         {
             if (obj == null) throw new ArgumentNullException("obj");
--- src/JE.ApiValidation.Tests/RequestValidity/OpenRasta/WhenFluentValidationOperationInterceptorRuns.cs
+++ src/JE.ApiValidation.Tests/RequestValidity/OpenRasta/WhenFluentValidationOperationInterceptorRuns.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using Moq;
 using OpenRasta.Binding;
 using OpenRasta.DI;
 using OpenRasta.Hosting.InMemory;
