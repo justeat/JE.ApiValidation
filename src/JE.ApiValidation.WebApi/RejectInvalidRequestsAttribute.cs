eb8d7c557aba445380e4fd844610fcdf1e210428
Peter Mounce <peter.mounce@just-eat.com>
2015-06-05 15:43:37 +0100

    COGCON-10: WebApi ModelState validation support
    Anything that inserts itself into WebApi's modelstate validation configuration will now respond with this shape of response.
    This includes FluentValidation validation and DataAnnotation attributes. The former is preferred.

---
+++ src/JE.ApiValidation.WebApi/RejectInvalidRequestsAttribute.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using JE.ApiValidation.DTOs;
+
+namespace JE.ApiValidation.WebApi
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    // ReSharper disable UnusedMember.Global
+    public abstract class RejectInvalidRequestsAttribute : ActionFilterAttribute
+        // ReSharper restore UnusedMember.Global
+    {
+        public override void OnActionExecuting(HttpActionContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                RespondWithBadRequest(context);
+            }
+        }
+
+        public override Task OnActionExecutingAsync(HttpActionContext context, CancellationToken cancellationToken)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                RespondWithBadRequest(context);
+            }
+            return base.OnActionExecutingAsync(context, cancellationToken);
+        }
+
+        private void RespondWithBadRequest(HttpActionContext context)
+        {
+            var response = new ResponseForInvalidRequest(context.ModelState);
+            LogBadRequest("Request pre-condition(s) failed", context, response);
+            // we intentionally don't use context.Request.CreateErrorResponse, so that we can customise the response body. Otherwise it's too limited.
+            context.Response = context.Request.CreateResponse(HttpStatusCode.BadRequest, response);
+        }
+
+        protected abstract void LogBadRequest(string message, HttpActionContext context, StandardErrorResponse response);
+    }
+}
---
+++ src/JE.ApiValidation.WebApi/ResponseForInvalidRequest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http.ModelBinding;
+using JE.ApiValidation.DTOs;
+
+namespace JE.ApiValidation.WebApi
+{
+    public class ResponseForInvalidRequest : StandardErrorResponse
+    {
+        public ResponseForInvalidRequest(ModelStateDictionary modelState)
+            : base((int)ErrorCodes.RequestWasInvalid, "The request is invalid.")
+        {
+            SerializeFromModelState(modelState);
+        }
+
+        private void SerializeFromModelState(ModelStateDictionary modelState)
+        {
+            foreach (var keyModelStatePair in modelState)
+            {
+                var key = keyModelStatePair.Key;
+                var errors = keyModelStatePair.Value.Errors;
+                if (errors == null || errors.Count <= 0)
+                {
+                    continue;
+                }
+
+                IList<Problem> problems = errors.Select(error => string.IsNullOrWhiteSpace(error.ErrorMessage)
+                                                                     ? new Problem { Message = "Undefined error." }
+                                                                     : new Problem { Message = error.ErrorMessage }).ToList();
+                Errors.Add(key, problems);
+            }
+        }
+    }
+}
