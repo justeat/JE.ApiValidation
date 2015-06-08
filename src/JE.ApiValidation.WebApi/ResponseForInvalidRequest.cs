4ac6be3fafee8ac7215613e20d2c6f9a11e42759
Peter Mounce <peter.mounce@just-eat.com>
2015-06-08 22:30:00 +0100

    Missed DataContract attribute

--- src/JE.ApiValidation.WebApi/ResponseForInvalidRequest.cs
+++ src/JE.ApiValidation.WebApi/ResponseForInvalidRequest.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Web.Http.ModelBinding;
 using JE.ApiValidation.DTOs;
 
 namespace JE.ApiValidation.WebApi
 {
+    [DataContract]
     public class ResponseForInvalidRequest : StandardErrorResponse
     {
         public ResponseForInvalidRequest(ModelStateDictionary modelState)
