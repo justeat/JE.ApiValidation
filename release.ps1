f3fec388e7b5ccb1518df7f77953dc01109f6c07
Peter Mounce <peter.mounce@just-eat.com>
2015-07-03 10:43:52 +0100

    make releasing easier

--- release.ps1
+++ release.ps1
@@ -1,8 +1,6 @@
 param(
   [Parameter(Mandatory=$true, HelpMessage="The version number to publish, eg 1.2.3. Set this in CI first.")]
   [string] $version,
-  [Parameter(Mandatory=$false, HelpMessage="CI project name")]
-  [string] $name = "je-apivalidation",
   [Parameter(Mandatory=$false, HelpMessage="CI project owner")]
   [string] $owner = "justeattech"
 )
@@ -11,26 +9,28 @@ if (($version -eq $null) -or ($version -eq '')) {
   # TODO: validate that a tag like this doesn't exist already
   throw "Must supply version number in semver format eg 1.2.3"
 }
-$ci = "https://ci.appveyor.com/project/$owner/$name"
+$manifest = get-content "deploy/manifest.json" -raw | ConvertFrom-Json
+$ci_name = "je-$($manifest.feature.name)"
+$ci_uri = "https://ci.appveyor.com/project/$owner/$ci_name"
 $tag = "v$version"
-$release = "release-$version"
+# $release = "release-$version"
 write-host "Your current status" -foregroundcolor green
 & git status
 write-host "Stashing any work and checking out master" -foregroundcolor green
 & git stash
 & git checkout master
+write-host "We'll pause now while you remember to bump the version number in CI ($ci_uri/settings) AND appveyor.yml to match the version you're releasing ($version) ;-)"
+read-host "hit enter when you've done that..."
 write-host "Tagging & branching. tag: $tag / branch: $release" -foregroundcolor green
 & git tag -a $tag -m "Release $tag"
 & git checkout $tag
-& git checkout -b $release
+# & git checkout -b $release
 # TODO: bounty - do this in code against api
 # http://www.appveyor.com/docs/api/projects-builds#update-project
-write-host "We'll pause now while you remember to bump the version number in CI ($ci/settings) AND appveyor.yml to match the version you're releasing ;-)"
-read-host "hit enter when you've done that..."
 write-host "Pushing" -foregroundcolor green
-& git push --tags upstream -u $release
+& git push --tags upstream# -u $release
 write-host "Done."
-write-host "Check $ci"
+write-host "Check $ci_uri"
 & git checkout master
 write-host "Putting you back on master branch" -foregroundcolor green
 exit 0
