18d56af008be69d061b92aef381213629cdf883d
Peter Mounce <peter.mounce@just-eat.com>
2015-07-10 11:15:56 +0100

    adjust for appveyor

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+using System.Reflection;
 
 [assembly: AssemblyDescription("API Validation support with FluentValidation for frameworks we use. Standard error response DTO.")]
 [assembly: AssemblyCompany("JUST EAT")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
