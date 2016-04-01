f57f2bd81451fe916966a9658afe013aee989b7c
saan800 <sandragreenhalgh@gmail.com>
2016-04-01 10:07:33 +0100

    Touch file to trigger AppVeyor build

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -3,3 +3,4 @@ using System.Reflection;
 [assembly: AssemblyDescription("API Validation support with FluentValidation for frameworks we use. Standard error response DTO.")]
 [assembly: AssemblyCompany("JUST EAT")]
 [assembly: AssemblyInformationalVersion("1.0.0.0")]
+ 
